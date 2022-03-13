6ea59fc8c482ddc6e0ac644789b71702b15ed8a8
trevin-livele <liveletrevin6@gmail.com>
2022-03-13 23:25:18 +0300

    added router urls

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing our ApiView"""
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializers a user profile objecct"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs =  {
+            'passwords': {
+                'write_only' : True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Ceate and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
+
+
+
+
+
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,15 @@
+from email import message
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+
+
+
+
 
 
 # Create your views here.
@@ -49,3 +56,56 @@ class HelloApiView(APIView):
         return Response({'method': 'DELETE'})
 
 
+class HelloViewSet(viewsets.ViewSet):
+    """Test Api viewset"""
+    serializer_class = serializers.HelloSerializer
+
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create,retrieve, update, partial_update)',
+            'Automatically maps to urls using routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+
+    def create(self, request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method' : 'PUT'})
+
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method' : 'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
